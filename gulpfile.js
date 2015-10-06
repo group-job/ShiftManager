18e021dfe423fefeee1972e9f3599f0e85466939
Shohe Masuyama <cisuco00@gmail.com>
2015-10-06 11:55:09 +0900

    css
    css

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,21 @@ var elixir = require('laravel-elixir');
  |
  */
 
+ var paths = {
+     'jquery': 'bower_components/jquery/',
+     'bootstrap': 'bower_components/bootstrap-sass-official/assets/'
+ };
+
 elixir(function(mix) {
     mix.less('app.less');
+    mix.sass('app.scss') // app.scssをコンパイルして、public/css/app.css に出力
+
+       // Bootstrapのフォントを public/fonts/bootstrapディレクトリにコピー
+       .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap')
+
+       // jquery.jsと bootstrap.jsを結合して、public/js/app.jsに出力
+       .scripts([
+            paths.jquery + "dist/jquery.js",
+            paths.bootstrap + "javascripts/bootstrap.js"
+        ], 'public/js/app.js', './');  // ①
 });
