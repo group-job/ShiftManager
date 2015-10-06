d743ac741a9de6b1135ee588b33d6d217c5e5a85
Shohe Masuyama <cisuco00@gmail.com>
2015-10-06 12:34:58 +0900

    contortller作成
    作成中

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>My Blog</title>
+
+    <!-- CSSを追加 --><!-- ① 追加 -->
+    <link rel="stylesheet" href="../public/css/app.css">
 </head>
+
 <body>
     <div class="container"><!-- ② 追加 -->
         @yield('content')
