d036bb5cb2f3cf95da3cc34103a86429261bf988
AkihiroYasunaga <Aki@Aki.local>
2015-10-27 22:51:18 +0900

    .

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,8 @@ var elixir = require('laravel-elixir');
 
  var paths = {
      'jquery': 'bower_components/jquery/',
-     'bootstrap': 'bower_components/bootstrap-sass-official/assets/'
+     'bootstrap': 'bower_components/bootstrap-sass-official/assets/',
+     'fullcalendar' : 'resources/assets/fullcalendar/'
  };
 
 elixir(function(mix) {
@@ -24,13 +25,15 @@ elixir(function(mix) {
       'home.scss'
     ]); // app.scssをコンパイルして、public/css/app.css に出力
 
-
        // Bootstrapのフォントを public/fonts/bootstrapディレクトリにコピー
        mix.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap');
 
        // jquery.jsと bootstrap.jsを結合して、public/js/app.jsに出力
        mix.scripts([
+            paths.fullcalendar+"moment.min.js",
             paths.jquery + "dist/jquery.js",
+            paths.fullcalendar+"jquery-ui.custom.min.js",
+            path.fullcalendar + "fullcalendar.min.js"
             paths.bootstrap + "javascripts/bootstrap.js"
         ], 'public/js/app.js', './');  // ①
 });
--- resources/views/home/personal-home.blade.php
+++ resources/views/home/personal-home.blade.php
@@ -2,9 +2,14 @@
 @section('title')
 ホーム
 @stop
-@section('main-contents')
+@section('title-space')
   <div class="col-md-offset-4 col-md-4"><h1>マイシフト</h1></div>
-
+@endsection
+@section('contens-space')
+  <script>
+    $('#calendar').fullCalendar();
+  </script>
+  <div id="calendar"></div>
 
 
 @endsection
