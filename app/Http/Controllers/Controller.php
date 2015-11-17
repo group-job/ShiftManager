89ee785c192951f38a2fc21b9d8f2462c75cd535
Shohe Masuyama <cisuco00@gmail.com>
2015-11-17 12:03:46 +0900

    t
    t

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,19 @@ use Illuminate\Routing\Controller as RoutingController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use App\Group;
+use View;
 
 abstract class Controller extends RoutingController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public function __construct()
+    {
+      //管理グループ取得
+      $manager_group = '';
+      View::share('manager_group',$manager_group);
+      $join_group = '';
+      View::share('join_group', $join_group);
+
+    }
 }
